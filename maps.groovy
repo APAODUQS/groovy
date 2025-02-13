aff6fb03e68c0a8c1417a90eadc3b7e1e7507ca1
Paola Duquino <64988351+APAODUQS@users.noreply.github.com>
2025-02-13 09:55:29 -0500

    Create maps.groovy

---
+++ maps.groovy
@@ -0,0 +1,10 @@
+def person = [first:"Dan", last:"Vega"]
+person.first
+person.new = "something"
+person.sort()
+person.size()
+person.keySet()
+person.values()
+
+// enum
+enum Days {SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY ,FRIDAY ,SATURDAY}
