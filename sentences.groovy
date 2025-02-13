0c370e2588baedc39830dd0f08fadcf4726ec5fa
Paola Duquino <64988351+APAODUQS@users.noreply.github.com>
2025-02-13 09:51:33 -0500

    Create sentences.groovy

---
+++ sentences.groovy
@@ -0,0 +1,32 @@
+// SENTENCES
+//if
+def x = 0
+if( x == 1) {
+  println x
+} else {
+  println ""
+}
+
+//while
+def i = 0
+while ( i <= 1) {
+  println i
+  i++
+}
+
+//for
+def list = [1,2,3,4]
+for( num in list) {
+  println num
+}
+
+list.each { println it}
+
+//switch
+switch(x) {
+  case 1:
+          println ""
+          case
+  default:
+          println ""
+}
