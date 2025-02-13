2cb00097c0e7936c41fcd13c23b4fc0cbe86feba
Paola Duquino <64988351+APAODUQS@users.noreply.github.com>
2025-02-13 09:54:40 -0500

    Create array.groovy

---
+++ array.groovy
@@ -0,0 +1,12 @@
+List num = [0,1,2,3]
+num.push(99)
+num.putAt(0,77)
+num(0) = 7
+nums << 66 //add last value push
+nums.pop() //remove last
+nums.removeAt(0)
+nums - 1 //remove 1 of all
+nums.getAt(0..3)
+nums.flatten() //if array inside array
+nums.unique() //remove duplication
+nums as SortedSet //order
